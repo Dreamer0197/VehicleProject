c6401aaa35d3ae1157bef5e1791875be23c75eac
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 21:56:59 +0300

    Finish

--- VehicleProject/main.cpp
+++ VehicleProject/main.cpp
@@ -6,9 +6,54 @@
 //
 
 #include <iostream>
+#include "Vehicle.h"
+using namespace std;
+
+
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    Vehicle *vPtr[]={new Car(125.850,"BMW"),new MotorCycle(37.500,"Honda")};
+    int which_vehicle;
+    char inputVehicle;
+    do{
+        fflush(stdin);
+        cout<<"Enter vehicle and input:";
+        cin>>which_vehicle>>inputVehicle;
+        switch (inputVehicle) {
+            case 'U':
+                vPtr[which_vehicle]->increaseSpeed();
+                vPtr[which_vehicle]->displayInformation();
+                break;
+                
+            case 'D':
+                vPtr[which_vehicle]->decreaseSpeed();
+                vPtr[which_vehicle]->displayInformation();
+                break;
+            case 'C':
+                vPtr[which_vehicle]->stopEngine();
+                vPtr[which_vehicle]->displayInformation();
+                break;
+            case 'S':
+                vPtr[which_vehicle]->startEngine();
+                vPtr[which_vehicle]->displayInformation();
+                break;
+            default:
+                cout<<"Enter valid input:"<<endl;
+                break;
+        }
+        
+    }while(which_vehicle!=-1);
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
